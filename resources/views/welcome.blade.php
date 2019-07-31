034f6eb7de6f7e3ae8f8bf416d78ee07d2ccd220
juggernaut adam <adamsobary05@gmail.com>
2019-07-31 08:44:00 +0700

    sds

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                Laravell
+                Laravel
                 </div>
 
                 <div class="links">
