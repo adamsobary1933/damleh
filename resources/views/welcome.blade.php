5b2b9aab1209de205e602b12db29ef686aa7965c
juggernaut adam <adamsobary05@gmail.com>
2019-07-31 09:00:51 +0700

    sd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                Laravell
+               Pembelian Obat
                 </div>
 
                 <div class="links">
