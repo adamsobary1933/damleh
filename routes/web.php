302db64043bdcccbd19b4966126464c039b214f7
juggernaut adam <adamsobary05@gmail.com>
2019-08-03 07:45:41 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,6 @@ Route::get('/', function(){
     return view('welcome');
 });
 
-//Route Buku
-Route::prefix('buku')->group(function () {
-    Route::get('/', 'BukuController@index')->name('index.buku');
-    Route::get('/add', 'BukuController@create')->name('add.buku');
-    Route::post('/store', 'BukuController@store')->name('store.buku');
-    Route::get('/detail/{id}', 'BukuController@show')->name('detail.buku');
-    Route::get('/edit/{id}', 'BukuController@edit')->name('edit.buku');
-	Route::post('/update/{id}', 'BukuController@update')->name('update.buku');
-	Route::get('/delete/{id}', 'BukuController@destroy')->name('delete.buku');
-    Route::get('/q', 'BukuController@index')->name('q.buku');
-});
 Auth::routes();
 
 Route::get('home', 'HomeController@index')->name('home');
